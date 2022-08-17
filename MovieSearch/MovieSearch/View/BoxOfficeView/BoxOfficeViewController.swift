cfc2b54659bb9413419786b287fc133f84d73390
ICS-Asan <carpediem1015@gmail.com>
2022-08-17 16:06:46 +0900

    feat: BoxOfficeViewController 생성

---
+++ MovieSearch/MovieSearch/View/BoxOfficeView/BoxOfficeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BoxOfficeViewController.swift
+//  MovieSearch
+//
+//  Created by Seul Mac on 2022/08/17.
+//
+
+import UIKit
+
+class BoxOfficeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
